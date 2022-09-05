9c60229e1713dbbb3dba83537f21f5918e29b3c9
alexlsilva7 <alex.lsilva154@gmail.com>
2022-09-05 13:09:16 -0300

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: ClipPath(
+          clipper: _CustomClipper(),
+          child: Container(
+              height: 150,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.indigo[900],
+              child: Center(
+                child: Text(
+                  'Explore',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              )),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
+
+class _CustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+
+    var path = Path();
+
+    path.lineTo(0, height - 50);
+    path.quadraticBezierTo(width / 2, height, width, height - 50);
+    path.lineTo(width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
---
+++ lib/screens/movie_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+  const MovieScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
