725d3d3c1fff03b63675def7ed6b4bd91c4074cb
Alex Lopes <alex.lsilva154@gmail.com>
2022-09-05 20:48:58 -0300

    home screen items

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_app_ui/components/movie_list_item.dart';
+import 'package:flutter_movie_app_ui/models/movie.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const List<Movie> movies = Movie.movies;
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 100,
@@ -17,18 +20,47 @@ class HomeScreen extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               color: Colors.indigo[900],
               child: Center(
-                child: Text(
-                  'Explore',
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Text(
+                    'Explore',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
                 ),
               )),
         ),
       ),
-      body: Container(),
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 120),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: TextSpan(
+                  style: Theme.of(context).textTheme.headline6,
+                  children: [
+                    TextSpan(
+                        text: 'Featured ',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(fontWeight: FontWeight.bold)),
+                    const TextSpan(text: 'Movies'),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              for (final movie in movies) MovieListItem(movie: movie)
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
