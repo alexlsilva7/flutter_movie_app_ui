cdfad3f0f90aaf9a0b030340a24f8e0efd5375c0
Alex Lopes <alex.lsilva154@gmail.com>
2022-09-06 20:18:01 -0300

    colocado transparencia na status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_movie_app_ui/screens/home_screen.dart';
 
 void main() {
@@ -11,6 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       title: 'Flutter Movie App UI',
       debugShowCheckedModeBanner: false,
